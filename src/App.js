f7bfc973349a6d687e20785730bf10fd97b11d19
jhery-z <herimaill4@gmail.com>
2022-09-11 05:22:02 +0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Detail from './components/Detail';
 import UserList from './components/UserList';
-
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Navbar/>
         <Route path="/" element={<UserList/>}/>
         <Route path="/detail/:id" element={<Detail/>}/>
       </Routes>
